2017789231f9d5281cbb7b2e4278a1381ccfe614
Haritz <haritzpuerto94@gmail.com>
2015-04-01 18:02:56 +0200

    Creado sinónimo notas_alumnos

--- Sinonimos.sql
+++ Sinonimos.sql
@@ -1,9 +1,15 @@
 CREATE PUBLIC SYNONYM mis_datos
 FOR docencia.mis_datos;
 GRANT SELECT ON mis_datos TO R_alumno;
+
 CREATE PUBLIC SYNONYM mis_notas
 FOR docencia.mis_notas;
 GRANT SELECT ON mis_notas TO R_alumno;
+
 CREATE PUBLIC SYNONYM mis_notas_de_ejercicios
 FOR docencia.mis_notas_de_ejercicios;
 GRANT SELECT ON mis_notas_de_ejercicios TO R_alumno;
+
+CREATE PUBLIC SYNONYM notas_alumnos
+FOR docencia.notas_alumnos;
+GRANT SELECT ON notas_alumnos TO r_administrativo;
