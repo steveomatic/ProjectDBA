dc49552833cedb5c48b324e2d7fb7926cbf87069
FelipeSulser <felipesulser@gmail.com>
2015-04-25 19:29:20 +0200

    Mejorado y con querrás de las buenas ademas para testar la corrección algunas nulo, algunas con comillas dobles etc etc

--- docencia2.sql
+++ docencia2.sql
@@ -232,34 +232,114 @@ CREATE BITMAP INDEX grupo_idx ON matricula(grupo) tablespace ts_index;
 CREATE BITMAP INDEX matricula_asignatura_idx ON matricula(asignatura_asignatura_id) tablespace ts_index;
 CREATE INDEX apellido_mayus_idx ON alumno(UPPER(apellido1)) tablespace ts_index;
 
-create sequence asignatura_seq start with 1 increment by 1;
-create sequence alumno_seq start with 1 increment by 1;
-create sequence ejercicio_seq start with 1 increment by 1;
-create sequence profesor_seq start with 1 increment by 1;
-create sequence relacion_seq start with 1 increment by 1;
-create sequence usuario_seq start with 1 increment by 1;
 
-INSERT INTO usuario VALUES(1,'Godel');
-INSERT INTO asignatura VALUES(1, 'ABD', 'Admin. Base de datos', '2cuat',50,100);
-INSERT INTO alumno VALUES (1, '12345678A', 'Haritz', 'Puerto', 'San Román', '0001', SYSDATE, TO_DATE('1994/10/26', 'yyyy/mm/dd'));
-INSERT INTO matricula VALUES('14/15', 'A', 1, 1, 1);
-INSERT INTO ejercicio VALUES(1, 1, 'Muestra sysdate', 'SELECT sysdate FROM dual;', 0, 1);
-INSERT into relacion VALUES (1,1,1,1);
-INSERT INTO calif_ejercicio VALUES(1,1,1,1,1, 'skfjlsdf');
-INSERT INTO ejercicio VALUES(2,1,'bla bla', 'sol', 0,1);
-INSERT INTO ejercicio VALUES(3,1,'bla bla', 'sol', 0,1);
-INSERT INTO ejercicio VALUES(4,1,'bla bla', 'sol', 0,1);
-INSERT INTO ejercicio VALUES(5,1,'bla bla', 'sol', 0,1);
-INSERT INTO calif_ejercicio VALUES(1,1,1,2,1,'fsafas');
-INSERT INTO calif_ejercicio VALUES(1,1,1,3,1,'asfasdf');
-INSERT INTO calif_ejercicio VALUES(0,1,1,4,1,'asdfasf');
-INSERT INTO calif_ejercicio VALUES(0,1,1,5,1,'asfddasf');
-INSERT INTO ejercicio VALUES(6,1,'bla bla', 'sol', 0,1);
-INSERT INTO ejercicio VALUES(7,1,'bla bla', 'sol', 0,1);
-INSERT INTO ejercicio VALUES(8,1,'bla bla', 'sol', 0,1);
-INSERT INTO ejercicio VALUES(9,1,'bla bla', 'sol', 0,1);
-INSERT into relacion VALUES (2,2,1,1);
-INSERT INTO calif_ejercicio VALUES(1,1,2,6,1,'adfas');
-INSERT INTO calif_ejercicio VALUES(1,1,2,7,1,'fgfhgg');
-INSERT INTO calif_ejercicio VALUES(0,1,2,8,1,'werwr');
-INSERT INTO calif_ejercicio VALUES(0,1,2,9,1,'rqwer');
+--al meter datos dummy,no usados en la realidad de la aplicacion hemos de comenzar por el 10 para evitar colisiones
+create sequence asignatura_seq start with 10 increment by 1;
+create sequence alumno_seq start with 10 increment by 1;
+create sequence ejercicio_seq start with 10 increment by 1;
+create sequence profesor_seq start with 10 increment by 1;
+create sequence relacion_seq start with 10 increment by 1;
+create sequence usuario_seq start with 10 increment by 1;
+
+
+
+insert into alumno(alumno_id,dni,nombre,apellido1,apellido2,expediente,fecha_alta,fecha_nacimiento) values(1,'999999X','Robert','Liam','Curtis','00001',sysdate,sysdate-20);
+insert into alumno(alumno_id,dni,nombre,apellido1,apellido2,expediente,fecha_alta,fecha_nacimiento) values(2,'787643X','Luke','Sky','Walker','00012',sysdate,sysdate-21);
+insert into alumno(alumno_id,dni,nombre,apellido1,apellido2,expediente,fecha_alta,fecha_nacimiento) values(3,'123445X','Jack','Kerouac',NULL,'00013',sysdate,sysdate-60);
+
+INSERT INTO asignatura(asignatura_id,codigo,nombre,cuatrimestre,min_puntos,max_puntos) VALUES(1, 'BD', 'Bases de Datos I', '2cuat',50,100);
+INSERT INTO asignatura(asignatura_id,codigo,nombre,cuatrimestre,min_puntos,max_puntos) VALUES(2, 'BD2', 'Bases de Datos 2', '1cuat',50,100);
+
+
+
+insert into usuario(usuario_id,nombre) values(1,'user_dummy1');
+insert into usuario(usuario_id,nombre) values(2,'user_dummy2');
+insert into usuario(usuario_id,nombre) values(3,'user_dummy3');
+
+
+
+INSERT INTO matricula(curso_academico,grupo,alumno_alumno_id,asignatura_asignatura_id,usuario_usuario_id) VALUES('14/15', 'A', 1, 1, 1);
+INSERT INTO matricula(curso_academico,grupo,alumno_alumno_id,asignatura_asignatura_id,usuario_usuario_id) VALUES('14/15', 'B', 2, 1, 2);
+INSERT INTO matricula(curso_academico,grupo,alumno_alumno_id,asignatura_asignatura_id,usuario_usuario_id) VALUES('14/15', 'A', 3, 2, 3);
+
+
+
+
+--Tablas donde los estudiantes ejecutaran sus querys en examenes o practica
+create table continent(
+continent_id number(5) primary key,
+continent_name varchar2(100) not null unique
+);
+create table countries(
+country_id number(5) primary key,
+country_name varchar2(100) not null unique,
+continent references continent(continent_id)
+);
+
+
+create table company(
+company_id number(5) primary key,
+company_name varchar2(100) not null,
+country_id references countries(country_id)
+);
+insert into continent values(1,'Africa');
+insert into continent values(2,'Europe');
+insert into continent values(3,'America');
+
+insert into countries values(1,'USA',3);
+insert into countries values(2,'France',2);
+insert into countries values(3,'UK',2);
+insert into countries values (4,'Ghana',1);
+insert into countries values (5,'Switzerland',2);
+insert into countries values (6,'Canada',3);
+
+insert into company values(1,'IBM',1);
+insert into company values(2,'Roche',5);
+insert into company values(3,'Carrefour',2);
+insert into company values(4,'Raytheon',1);
+insert into company values(5,'THIS_IS_INVENTED',4);
+insert into company values(6,'Airbus',2);
+
+
+grant select on continent to R_ALUMNO;
+grant select on countries to R_ALUMNO;
+grant select on company to R_ALUMNO;
+
+INSERT INTO ejercicio(ejercicio_id,tema,enunciado,solucion,fallos,retribucion,palabras_clave) VALUES(1, 1, 'Muestra sysdate', 'SELECT sysdate FROM dual', 0, 1,'sysdate, dual');
+insert into ejercicio(ejercicio_id,tema,enunciado,solucion,fallos,retribucion,palabras_clave) values(2,2,'Selecciona todos los continentes','Select * from continent', 0,1,'continent, easy');
+
+
+
+insert into ejercicio(ejercicio_id,tema,enunciado,solucion,fallos,retribucion,palabras_clave) values(3,2,'Selecciona todos los paises en america','select * from countries
+where continent in(select continent_id from continent where continent_name = ''America'')', 0,1,'countries, hard');
+
+insert into ejercicio(ejercicio_id,tema,enunciado,solucion,fallos,retribucion,palabras_clave) values(4,2,'Selecciona todos los paises que empiecen por S','select * from countries
+where country_name like ''S%''', 0,1,'wildcard, hard');
+
+
+insert into relacion(relacion_id,tema,usuario_usuario_id,asignatura_asignatura_id) values(4,1,1,1);
+insert into relacion(relacion_id,tema,usuario_usuario_id,asignatura_asignatura_id) values(5,2,2,1);
+insert into relacion(relacion_id,tema,usuario_usuario_id,asignatura_asignatura_id) values(6,2,3,2);
+
+--cada usuario ahora tiene una relacion, ahora asignamos los ejercicios con calif_ejercicio
+
+
+--cuando el usuario responda, se actualizara a otra cosa que null y 
+--cuando acabe el tiempo, el profesor ejecutara 
+--el procedimiento de correccion
+
+INSERT INTO calif_ejercicio(nota,usuario_usuario_id,relacion_relacion_id,ejercicio_ejercicio_id,asignatura_id,respuesta) 
+VALUES(0,1,4,1,1,'select sysdate from dual');
+
+INSERT INTO calif_ejercicio(nota,usuario_usuario_id,relacion_relacion_id,ejercicio_ejercicio_id,asignatura_id,respuesta) 
+VALUES(0,2,5,2,1,'select * from continent');
+
+INSERT INTO calif_ejercicio(nota,usuario_usuario_id,relacion_relacion_id,ejercicio_ejercicio_id,asignatura_id,respuesta) 
+VALUES(0,2,5,3,1,NULL);
+
+INSERT INTO calif_ejercicio(nota,usuario_usuario_id,relacion_relacion_id,ejercicio_ejercicio_id,asignatura_id,respuesta) 
+VALUES(0,3,6,3,2,'selec * from dual');
+
+INSERT INTO calif_ejercicio(nota,usuario_usuario_id,relacion_relacion_id,ejercicio_ejercicio_id,asignatura_id,respuesta) 
+VALUES(0,3,6,4,2,'select * from countries
+where country_name like ''S%''');  
