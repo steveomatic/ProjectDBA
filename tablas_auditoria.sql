ec15623aa584ebf90527a60410c605b262801f75
Esteban <stedeg_3@hotmail.com>
2015-04-29 18:23:22 +0200

    Actualizada tabla para que funcione con script

--- tablas_auditoria.sql
+++ tablas_auditoria.sql
@@ -1,8 +1,8 @@
-CREATE TABLE acceso_ejercicios
+CREATE TABLE audit_ejer
   (
   usuario_id  NUMBER NOT NULL,
   ejercicio_id NUMBER NOT NULL,
   fecha_inicio DATE NOT NULL,
   fecha_entrega DATE
   );
-  ALTER TABLE acceso_ejercicios ADD CONSTRAINT USUARIO_UNIQUE UNIQUE (usuario_id, ejercicio_id);
+  ALTER TABLE audit_ejer ADD CONSTRAINT USUARIO_UNIQUE UNIQUE (usuario_id, ejercicio_id);
