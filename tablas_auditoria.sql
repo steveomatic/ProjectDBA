8ee588fabb1865338c522190ef14ebb11ffa6f17
Esteban <stedeg_3@hotmail.com>
2015-05-01 17:30:41 +0200

    Añadida columna fecha_entrega_ultima
    Registrará cualquier update.

--- tablas_auditoria.sql
+++ tablas_auditoria.sql
@@ -3,6 +3,7 @@ CREATE TABLE audit_ejer
   usuario_id  NUMBER NOT NULL,
   ejercicio_id NUMBER NOT NULL,
   fecha_inicio DATE NOT NULL,
-  fecha_entrega DATE
+  fecha_entrega_ultima DATE,
+  fecha_entrega_correcto DATE
   );
   ALTER TABLE audit_ejer ADD CONSTRAINT USUARIO_UNIQUE UNIQUE (usuario_id, ejercicio_id);
