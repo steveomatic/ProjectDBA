1629033f0dd4c5220984ddf42a685a8beb6b440c
Haritz <haritzpuerto94@gmail.com>
2015-05-01 12:55:43 +0200

    añadido mensaje de ok

--- CORREC_EJER.sql
+++ CORREC_EJER.sql
@@ -171,21 +171,22 @@ PACKAGE BODY CORREC_EJER AS
   
 
   procedure asignacion_ejer(usuario_id in number, relacion_id in number, asignatura_id in number, ejercicio_id in number)as
-   ERROR_PRIVS_INSUF exception;
+  ERROR_PRIVS_INSUF exception;
   ERROR_DESCONOCIDO exception;
-  begin
-  begin
-  insert into docencia.calif_ejercicio(nota,usuario_usuario_id,relacion_relacion_id,ejercicio_ejercicio_id,asignatura_id,respuesta) values(0,usuario_id,relacion_id,ejercicio_id,asignatura_id,null);
-  EXCEPTION WHEN OTHERS THEN 
-          IF SQLCODE = -1031 then raise ERROR_PRIVS_INSUF;
-          ELSE raise ERROR_DESCONOCIDO;
-          END IF;
-end;
- EXCEPTION
+    begin
+      begin
+        insert into docencia.calif_ejercicio(nota,usuario_usuario_id,relacion_relacion_id,ejercicio_ejercicio_id,asignatura_id,respuesta) values(0,usuario_id,relacion_id,ejercicio_id,asignatura_id,null);
+        DBMS_OUTPUT.put_line('Asignación satisfactoria');
+      EXCEPTION WHEN OTHERS THEN 
+        IF SQLCODE = -1031 then raise ERROR_PRIVS_INSUF;
+        ELSE raise ERROR_DESCONOCIDO;
+        END IF;
+    end;
+  EXCEPTION
     WHEN ERROR_PRIVS_INSUF THEN DBMS_OUTPUT.put_line('Error: no se tienen privilegios suficientes');
    
     WHEN ERROR_DESCONOCIDO THEN DBMS_OUTPUT.put_line('Error desconocido');
-    end asignacion_ejer;
+  end asignacion_ejer;
   
   ---------------------------------------------------------------------------------------------------------------------------
   ---------------------------------------------------------------------------------------------------------------------------
