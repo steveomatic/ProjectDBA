cdfee4c1cb6a4c547456de9dc439ec538867b274
FelipeSulser <felipesulser@gmail.com>
2015-05-05 13:31:00 +0200

    Cambiado el formato DATE a TIMESTAMP, nos interesan los segundos no los días ( En date no se cuentan los segundos).
    Añadido procedimiento para que el alumno responda

--- tablas_auditoria.sql
+++ tablas_auditoria.sql
@@ -2,7 +2,8 @@ CREATE TABLE audit_ejer
   (
   usuario_id  NUMBER NOT NULL,
   ejercicio_id NUMBER NOT NULL,
-  fecha_inicio DATE NOT NULL,
-  fecha_entrega DATE
+  fecha_inicio timestamp NOT NULL,
+  fecha_entrega_ultima timestamp,
+  fecha_entrega_correcto timestamp
   );
   ALTER TABLE audit_ejer ADD CONSTRAINT USUARIO_UNIQUE UNIQUE (usuario_id, ejercicio_id);
