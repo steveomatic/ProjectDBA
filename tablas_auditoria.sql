5ecf22556922739e4054b57a203cefa916949f94
Esteban <stedeg_3@hotmail.com>
2015-05-05 17:58:10 +0200

    para poder mergear

--- tablas_auditoria.sql
+++ tablas_auditoria.sql
@@ -1,9 +1,1 @@
-CREATE TABLE audit_ejer
-  (
-  usuario_id  NUMBER NOT NULL,
-  ejercicio_id NUMBER NOT NULL,
-  fecha_inicio TIMESTAMP NOT NULL,
-  fecha_entrega_ultima TIMESTAMP,
-  fecha_entrega_correcto TIMESTAMP
-  );
-  ALTER TABLE audit_ejer ADD CONSTRAINT USUARIO_UNIQUE UNIQUE (usuario_id, ejercicio_id);
+
