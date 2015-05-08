94a42dd53ea29d7091e7118bb9572621ba2a6a0d
Haritz <haritzpuerto94@gmail.com>
2015-05-08 12:05:56 +0200

    Terminado antiplagio semántico.

--- antiplagio.sql
+++ antiplagio.sql
@@ -1,25 +1,34 @@
-CREATE OR REPLACE
+create or replace 
 PACKAGE BODY ANTIPLAGIO AS
 
-  PROCEDURE semantico(usuario_id INTEGER, asignatura_id INTEGER, ejercicio_id INTEGER) AS
+  PROCEDURE semantico(usuario_id INTEGER, asignatura INTEGER, relacion_id INTEGER, ejercicio_id INTEGER) AS
+  --ERROR_ALUMNO_HA_COPIADO EXCEPTION;
   respuesta_alu DOCENCIA.CALIF_EJERCICIO.RESPUESTA%TYPE;
-  respuesta_copiada INTEGER; -- Almacenará 0 si se ha copiado
+  ha_copiado INTEGER; -- Almacenará 1 si se ha copiado
   CURSOR c_respuestas IS -- Todas las respuestas al ejercicio "ejercicio_id" excepto la del estudiante usuario_id
     SELECT respuesta FROM calif_ejercicio
-    WHERE usuario_usuario_id != usuario_id AND asignatura_id = asignatura_id; 
+    WHERE usuario_usuario_id != usuario_id AND asignatura = asignatura_id AND ejercicio_ejercicio_id = ejercicio_id; 
   
   BEGIN
     SELECT respuesta INTO respuesta_alu FROM calif_ejercicio -- Cogemos la respuesta del estudiante y la metemos en respuesta_alu
-      WHERE usuario_usuario_id = usuario_id AND asignatura_id = asignatura_id; 
+      WHERE usuario_usuario_id = usuario_id AND asignatura_id = asignatura AND relacion_relacion_id = relacion_id AND ejercicio_ejercicio_id = ejercicio_id; 
+    ha_copiado := 0;
     FOR respuesta IN c_respuestas LOOP
-      --SELECT UPPER(REPLACE(respuesta, ' ')) = UPPER(respuesta_alu) INTO respuesta_copiada FROM DUAL;
-      IF UPPER(REPLACE(respuesta, ' ')) = UPPER(respuesta_alu) THEN
-        dbms_output.put_line('Es muy posible que se haya copiado. Hay que indicarlo en alguna tabla');
-        --INDICARLO EN ALGUNA TABLA
-      END IF;
-    
+        -- Comparo la respuesta del alumno con las respuestas dadas por otros alumnos al mismo ejericio. Quito espacios y lo pongo en mayúsculas.
+        IF UPPER(REPLACE(respuesta.respuesta, ' ')) = UPPER(REPLACE(respuesta_alu, ' ')) THEN 
+          dbms_output.put_line('Es muy posible que se haya copiado.');
+          ha_copiado := 1;
+          EXIT;
+        END IF;
     END LOOP;
-
+    
+    IF ha_copiado = 0 THEN
+      dbms_output.put_line('No ha copiado.');
+    END IF;
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN dbms_output.put_line('El alumno no ha respondido a esa pregunta.');
+      WHEN OTHERS THEN dbms_output.put_line('Error desconocido.');
+        
   END semantico;
 
 END ANTIPLAGIO;
