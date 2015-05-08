be8ba909f2a3e6712e1b3d83865ee38a8fe25506
Felipe Sulser <felipesulser@gmail.com>
2015-05-08 17:55:52 +0200

    Update ESTADISTICAS_ALU.sql

--- ESTADISTICAS_ALU.sql
+++ ESTADISTICAS_ALU.sql
@@ -1,14 +1,16 @@
 create or replace 
 PACKAGE BODY ESTADISTICAS_ALU AS
 
-  procedure MAS_FALLOS AS
-   ejer_id ejercicio.ejercicio_id%type;
-   ejercicio_enunciado ejercicio.enunciado%type;
-   ejercicio_fallos ejercicio.fallos%type;
-   CURSOR ejer_cur is 
-       select ejercicio_id,enunciado,fallos from ejercicio
-       where fallos in (select max(fallos) from ejercicio);
-    BEGIN  
+   PROCEDURE MAS_FALLOS AS
+   ejer_id docencia.ejercicio.ejercicio_id%type;
+   ejercicio_enunciado docencia.ejercicio.enunciado%type;
+   ejercicio_fallos docencia.ejercicio.fallos%type;
+   CURSOR ejer_cur is -- Cursor que tiene los ejercicios donde más se ha fallado (todos tienen el mismo nº de fallos)
+       select ejercicio_id,enunciado,fallos from docencia.ejercicio
+       where fallos in (select max(fallos) from docencia.ejercicio);
+       
+    BEGIN
+       /* Otra forma propuesta por felipe. La veo más complicada. Fdo: Haritz Beck
        OPEN ejer_cur;
        LOOP
           FETCH ejer_cur into ejer_id, ejercicio_enunciado,ejercicio_fallos;
@@ -16,9 +18,20 @@ PACKAGE BODY ESTADISTICAS_ALU AS
           dbms_output.put_line('ID= '||ejer_id || ' ' || ejercicio_enunciado || ' #Fallos=' ||ejercicio_fallos);
        END LOOP;
        CLOSE ejer_cur;
+       */
+       FOR ejercicio IN ejer_cur LOOP
+          ejer_id := ejercicio.ejercicio_id;
+          ejercicio_enunciado := ejercicio.enunciado;
+          ejercicio_fallos := ejercicio.fallos;
+          dbms_output.put_line('ID= '||ejer_id || ' ' || ejercicio_enunciado || ' #Fallos=' ||ejercicio_fallos);
+       END LOOP;
+       
+       IF ejer_cur%ISOPEN = TRUE THEN 
+        CLOSE ejer_cur;
+       END IF;
        
        EXCEPTION
-       when others then
+       WHEN others then
        dbms_output.put_line('Error, no se ha podido encontrar los ejercicios');
   END MAS_FALLOS;
   
