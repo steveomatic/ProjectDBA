9c10ccfbc9c91964ec968c6fe0c89cce9793eac5
Haritz <haritzpuerto94@gmail.com>
2015-05-08 14:31:49 +0200

    Fixed responder. Ahora el alumno solo puede responder sus preguntas

--- PROC_ALU.sql
+++ PROC_ALU.sql
@@ -242,12 +242,16 @@ procedure correccion_alu(cor_usuario_id in number,cor_relacion_id in number , co
   
  
 
-  procedure responder(res_respuesta in varchar2,res_usuario_id in number,res_relacion_id in number , res_ejercicio_id in number, res_asignatura_id in number) as
+procedure responder(res_respuesta in varchar2,res_relacion_id in number , res_ejercicio_id in number, res_asignatura_id in number) as
     ERROR_PRIVS_INSUF exception;
     ERROR_DESCONOCIDO exception;
     respuesta_filtrada DOCENCIA.EJERCICIO.SOLUCION%TYPE;
+    res_usuario_id NUMBER;
     begin
     
+      SELECT usuario_id INTO res_usuario_id -- este select coge el usuario_id del que llama al procedure
+      FROM DOCENCIA.usuario
+      WHERE UPPER(usuario.nombre) = UPPER(user);  
       begin
         --[DEPRECATED] Si el último carácter es un ; entonces lo elimina, si no, nada. 
         --IF SUBSTR(res_respuesta, -1) = ';' THEN respuesta_filtrada := SUBSTR(res_respuesta, 0,length(res_respuesta)-1);
@@ -270,19 +274,22 @@ procedure correccion_alu(cor_usuario_id in number,cor_relacion_id in number , co
         correccion_alu(res_usuario_id,res_relacion_id,res_ejercicio_id, res_asignatura_id);
         DBMS_OUTPUT.put_line('Respuesta autoevaluada');
         EXCEPTION WHEN OTHERS THEN 
-        IF SQLCODE = -1031 then raise ERROR_PRIVS_INSUF;
-        ELSE raise ERROR_DESCONOCIDO;
-        END IF;
+          IF SQLCODE = -1031 then raise ERROR_PRIVS_INSUF;
+          ELSE raise ERROR_DESCONOCIDO;
+          END IF;
       end;
      
     exception
-    when ERROR_PRIVS_INSUF then DBMS_OUTPUT.put_line('Error: no se tienen privilegios suficientes');
-   
-    when ERROR_DESCONOCIDO then DBMS_OUTPUT.put_line('Error desconocido');
+      when ERROR_PRIVS_INSUF then DBMS_OUTPUT.put_line('Error: no se tienen privilegios suficientes');
+      when ERROR_DESCONOCIDO then DBMS_OUTPUT.put_line('Error desconocido correccion');
+      
     end responder;
     
     
-    --para cada relacion, visualiza cada pregunta
+    
+
+
+--para cada relacion, visualiza cada pregunta
 procedure ver_preguntas(ver_usuario_id in number, ver_relacion_id in number, ver_asignatura_id in number) as
 
    ejer_id  ejercicio.ejercicio_id%type;
