52a4afeac89246cedbacc277dc4a2df2082fdfbf
AlvaroFaure <alvarogfaure@gmail.com>
2015-05-09 19:31:29 +0200

    Corregido el procedimiento N_MEJORES_ASIGNATURA

--- MEJORES_ALUMNOS.sql
+++ MEJORES_ALUMNOS.sql
@@ -4,21 +4,23 @@ PACKAGE BODY MEJORES_ALUMNOS AS
   PROCEDURE N_MEJORES_ASIGNATURA(asig_id in number, num in number) IS
   
   CURSOR alumnos_cursor is
-    SELECT usuario.nombre usuario FROM calif_ejercicio, usuario
-    where asignatura_id=asig_id
-    GROUP BY usuario.nombre
-    order by sum(Nota);
+   SELECT usuario_id us, sum(nota) n FROM calif_ejercicio, usuario
+    where asignatura_id=1 and usuario_id=usuario_usuario_id
+    GROUP BY usuario_usuario_id
+    order by sum(Nota) desc;
     
   usuario_nombre USUARIO.NOMBRE%TYPE;
   cont number :=1;
+  puntos number;
   
   BEGIN
   
   FOR al_var in alumnos_cursor LOOP
       
     IF cont<=num  THEN
-    usuario_nombre := al_var.usuario;
-    DBMS_OUTPUT.PUT_LINE(cont||'. '||usuario_nombre);
+    select u.nombre into usuario_nombre from usuario u where usuario_id=al_var.us;
+    puntos := al_var.n;
+    DBMS_OUTPUT.PUT_LINE(cont||'. '||usuario_nombre ||' con ' || puntos || ' puntos');
     cont := cont+1;
     END IF;
     
