65a6accd60c7502768aa04f9bd1ca9ce9a002c03
Esteban <stedeg_3@hotmail.com>
2015-05-09 14:13:21 +0200

    Agregado campos que nos faltaban
    RELACION_ID
    ASIGNATURA_ID
    
    y puesto como unique la combinación de USUARIO, EJERCICIO, RELACION y ASIGNATURA

--- tablas_auditoria.sql
+++ tablas_auditoria.sql
@@ -2,8 +2,10 @@ CREATE TABLE audit_ejer
   (
   usuario_id  NUMBER NOT NULL,
   ejercicio_id NUMBER NOT NULL,
+  relacion_id NUMBER NOT NULL,
+  asignatura_id NUMBER NOT NULL,
   fecha_inicio timestamp NOT NULL,
   fecha_entrega_ultima timestamp,
   fecha_entrega_correcto timestamp
   );
-  ALTER TABLE audit_ejer ADD CONSTRAINT USUARIO_UNIQUE UNIQUE (usuario_id, ejercicio_id);
+  ALTER TABLE audit_ejer ADD CONSTRAINT USUARIO_UNIQUE UNIQUE (usuario_id, ejercicio_id, relacion_id, asignatura_id);
