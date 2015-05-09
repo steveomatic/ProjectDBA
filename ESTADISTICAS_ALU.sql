bc6f1f9865d690a2c202a6424706fb10c72ad893
Haritz <haritzpuerto94@gmail.com>
2015-05-09 22:56:17 +0200

    Añadido n mejores a estadisitcas_alu

--- ESTADISTICAS_ALU.sql
+++ ESTADISTICAS_ALU.sql
@@ -35,7 +35,59 @@ PACKAGE BODY ESTADISTICAS_ALU AS
        dbms_output.put_line('Error, no se ha podido encontrar los ejercicios');
   END MAS_FALLOS;
   
+
+  ----------------------------------------------------------------------------------------------------------------------------
+  ----------------------------------------------------------------------------------------------------------------------------
+  ----------------------------------------------------------------------------------------------------------------------------
+  ----------------------------------------------------------------------------------------------------------------------------
+
   
+  --Este procedimiento recibe el identificador de una asignatura y un número N
+  --y devuelve los N alumnos con mayor cantidad de puntos de esa asignatura
+  PROCEDURE N_MEJORES_ASIGNATURA(asig_id in number, N in number) IS
+  
+  --El cursor recoge la lista de alumnos de esa asignatura junto a sus notas
+  --Ordenados de mayor a menor nota
+  CURSOR alumnos_cursor is
+   select nombre, sum(nota) n from notas_alumnos_para_procedure where asignaturaID=asig_id
+   GROUP BY  nombre, asignatura ORDER BY SUM(nota) desc;
+    
+  usuario_nombre ALUMNO.NOMBRE%TYPE;  --Almacena el nombre del usuario en el bucle
+  cont number :=1;   --Contador para mostrar por pantalla el top y para controlar el límite N
+  puntos number;     --Variable que almacena el número de puntos del usuario en el bucle
+  nombre_asignatura docencia.asignatura.nombre%type;
+
+  BEGIN
+
+   select nombre into nombre_asignatura from asignatura where asignatura_id=asig_id;
+   DBMS_OUTPUT.PUT_LINE('Los mejores alumnos de la asignatura '|| nombre_asignatura ||' son: ');
+  
+  --Se recorre la lista de alumnos y se muestran por pantalla los N primeros
+  FOR al_var in alumnos_cursor LOOP
+      
+    IF cont<=N  THEN
+    usuario_nombre := al_var.nombre;
+    puntos := al_var.n;
+    --Ejemplo: 1. Usu Ariodep Rueba con 78 puntos
+    DBMS_OUTPUT.PUT_LINE(cont||'. '||usuario_nombre ||' con ' || puntos || ' puntos');
+    cont := cont+1;
+    ELSE
+      EXIT;
+    END IF;
+
+
+    
+  END LOOP;
+    
+  IF alumnos_cursor%ISOPEN THEN 
+     CLOSE alumnos_cursor;
+  END IF;
+    
+  EXCEPTION
+    WHEN others then
+    DBMS_OUTPUT.PUT_LINE('Error, no se han podido obtener los mejores alumnos');
+  
+  END N_MEJORES_ASIGNATURA;
 
 
 END ESTADISTICAS_ALU;
