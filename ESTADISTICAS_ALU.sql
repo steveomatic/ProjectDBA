0109bf8f14699b621fafe69f055673db511c3cd1
FelipeSulser <felipesulser@gmail.com>
2015-05-09 17:11:49 +0200

    Added principal procedure for this purpose

--- ESTADISTICAS_ALU.sql
+++ ESTADISTICAS_ALU.sql
@@ -34,6 +34,106 @@ PACKAGE BODY ESTADISTICAS_ALU AS
        WHEN others then
        dbms_output.put_line('Error, no se ha podido encontrar los ejercicios');
   END MAS_FALLOS;
+
+  
+  
+  --Da un informe completo del alumno por asignatura
+  PROCEDURE ANALISIS_ALU_ASIGNATURA(alumno_id number, asignatura_id number) as
+  
+  nombre_alumno notas_alumnos.nombre%type;
+  num_relacion notas_alumnos.relacion%type;
+  nota_alu notas_alumnos.nota%type;
+  
+  v_nombre_alu notas_alumnos.nombre%type;
+  v_nombre_asignatura notas_alumnos.asignatura%type;
+  
+ 
+  
+  cursor med_cur(nombre_alu varchar2,asignatura_nombre varchar2) is
+  select mediana
+  from mediana_alu_relacion
+  where nombre = nombre_alu
+  and
+  asignatura= asignatura_nombre
+  ; 
+  cursor rel_cur is
+  select relacion,nota,nombre 
+  from notas_alumnos
+  where alumnoid = alumno_id
+  and
+  ASIGNATURAID = asignatura_id;
+
+no_datos_exception exception;
+demasiadas_tuplas_exception exception;
+  begin  
+  
+  
+  --truco de poner max - asi nos asegurarmos de que la query devuelva a lo sumo una fila
+  begin
+  select max(nombre)
+  into v_nombre_alu
+  from notas_alumnos where alumnoid = alumno_id;
+  exception
+  when NO_DATA_FOUND then raise no_datos_exception;
+  when TOO_MANY_ROWS then raise demasiadas_tuplas_exception;
+  end;
+  
+  begin
+  select max(asignatura)
+  into v_nombre_asignatura
+  from notas_alumnos
+  where ASIGNATURAID = asignatura_id;
+  exception
+  when NO_DATA_FOUND then raise no_datos_exception;
+  when TOO_MANY_ROWS then raise demasiadas_tuplas_exception;
+  end;
+  dbms_output.put_line('');
+  dbms_output.put_line('***************  '||v_nombre_alu ||' en la asignatura '||v_nombre_asignatura||'  ***************');
+  dbms_output.put_line('');
+
+  FOR tupla IN rel_cur LOOP
+    nombre_alumno := tupla.nombre;
+    num_relacion := tupla.relacion;
+    nota_alu := tupla.nota;
+          dbms_output.put_line(nombre_alumno || ': Relacion-> ' || num_relacion || ' Nota-> ' ||nota_alu);
+       END LOOP;
+       
+       IF rel_cur%ISOPEN = TRUE THEN 
+        CLOSE rel_cur;
+       END IF;
+       
+  FOR tupla2 in med_cur(v_nombre_alu,v_nombre_asignatura) LOOP
+      dbms_output.put_line('*****   Mediana Asignatura: '||v_nombre_asignatura||': '||tupla2.mediana||'     *****');
+      end loop;
+       IF med_cur%ISOPEN = TRUE THEN 
+        CLOSE med_cur;
+       END IF;
+      
+       
+      EXCEPTION
+      WHEN NO_DATA_FOUND THEN dbms_output.put_line('Aun no hay entradas para dicho alumno y/o asignatura.');
+      WHEN no_datos_exception then dbms_output.put_line('Aun no hay entradas para dicho alumno y/o asignatura.');
+      WHEN demasiadas_tuplas_exception then dbms_output.put_line('Error too many rows');
+      WHEN OTHERS THEN dbms_output.put_line('Error desconocido.');
+  end ANALISIS_ALU_ASIGNATURA;
+
+  PROCEDURE ANALISIS_ALU(alumno_id number) as
+  
+  cursor asignaturas_cur is
+  select asignatura_asignatura_id
+  from matricula
+  where alumno_alumno_id = alumno_id;
+  
+  begin
+    FOR tupla in asignaturas_cur LOOP
+      ANALISIS_ALU_ASIGNATURA(alumno_id,tupla.asignatura_asignatura_id);
+      end loop;
+       IF asignaturas_cur%ISOPEN = TRUE THEN 
+        CLOSE asignaturas_cur;
+       END IF;
+       EXCEPTION
+      WHEN OTHERS THEN dbms_output.put_line('Error desconocido.');
+  end ANALISIS_ALU;
   
   
 
